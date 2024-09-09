2bde7235bc46a9c465261a31abe3dd6c976a81ef
Priscilla Sequetin <55082190+priskaurdi@users.noreply.github.com>
2024-09-09 07:47:30 -0300

    cadastro-2024-09-09

--- apps/contas/views.py
+++ apps/contas/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import Group, User
+from contas.forms import CustomUserCreationForm 
 
 # Create your views here.
 
@@ -23,3 +25,26 @@ def login_view(request):
     if request.user.is_authenticated:
         return redirect('home')
     return render(request, 'login.html')
+
+#Registrar
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            usuario = form.save(commit=False)
+            usuario.is_valid = False
+            usuario.save()
+            
+            group = Group.objects.get(name='usuario')
+            usuario.groups.add(group)
+            
+            messages.success(request, 'Registrado. Agora faça o login para começar!')
+            return redirect('login')
+        else:
+            # Tratar quando usuario já existe, senhas... etc...
+            messages.error(request, 'A senha deve ter pelo menos 1 caractere maiúsculo, \
+                1 caractere especial e no minimo 8 caracteres.')
+    form = CustomUserCreationForm()
+    return render(request, "register.html",{"form": form})
