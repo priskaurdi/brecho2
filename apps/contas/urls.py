0089775a277bbd76a3180ec13e1091e8f6ba4d86
Priscilla Sequetin <55082190+priskaurdi@users.noreply.github.com>
2024-09-10 06:27:31 -0300

    permissoes-contas-usuario-2024-09-0

--- apps/contas/urls.py
+++ apps/contas/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path("", include("django.contrib.auth.urls")),  # Django auth
     path('timeout/',  views.timeout_view, name='timeout'),
     path('sair/', views.logout_view, name='logout'),
-    path('entrar/', views.login_view, name='login'), 
-    path('criar-conta/', views.register_view, name='register'), 
+    path('entrar/', views.login_view, name='login'),
+    path('criar-conta/', views.register_view, name='register'),
     path('atualizar-usuario/', views.atualizar_meu_usuario, name='atualizar_meu_usuario'),
     path('atualizar-usuario/<int:user_id>/', views.atualizar_usuario, name='atualizar_usuario'),
-    
+ 
 ]
