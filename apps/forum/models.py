7ad4e5e0a978cf488e20c0d45d4c898ea7442a2f
Priscilla Sequetin <55082190+priskaurdi@users.noreply.github.com>
2024-09-11 13:03:59 -0300

    anexos-imgs-posts-2024-09-11

--- apps/forum/models.py
+++ apps/forum/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ValidationError
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 from django.conf import settings
@@ -14,13 +15,26 @@ class PostagemForum(models.Model):
     data_publicacao = models.DateField(blank=True, null=True)
     data_criacao = models.DateTimeField(default=timezone.now)
     ativo = models.BooleanField('Publicar Postagem?', default=False)
-    anexar_imagem = models.ImageField('Imagem Anexo', 
-							upload_to='postagem-forum/', blank=True, null=True)
+    # anexar_imagem = models.ImageField('Imagem Anexo', upload_to='postagem-forum/', blank=True, null=True)
     
     def __str__(self):
         return "{} ({})".format(self.titulo, self.data_publicacao)
-
-		class Meta:
+    
+    class Meta:
         verbose_name = 'Postagem Forum'
         verbose_name_plural = 'Postagem Forum'
         ordering = ['-data_criacao']
+
+
+class PostagemForumImagem(models.Model):
+    imagem = models.FileField('Imagem Anexo', upload_to='postagem-forum/')
+    postagem = models.ForeignKey(PostagemForum, 
+					related_name='postagem_imagens', on_delete=models.CASCADE)
+ 
+    def __str__(self):
+        return self.postagem.titulo
+    
+    def clean(self):
+        super().clean()
+        if self.postagem.postagem_imagens.count() >= 5: # Limitar somente 5 anexos
+            raise ValidationError('Você só pode adicionar no máximo 5 anexos.')
