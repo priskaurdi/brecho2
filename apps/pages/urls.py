497871563ea8c99b12a0077af1781290ef512cfd
Priscilla Sequetin <55082190+priskaurdi@users.noreply.github.com>
2024-09-17 20:38:46 -0300

    layout-modelo-2024-09-17

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -3,4 +3,7 @@ from pages import views
 
 urlpatterns = [
     path('', views.index, name='home'), 
+    path('sobre/', views.paginas_view, name='sobre'), 
+    path('faq/', views.paginas_view, name='faq'), 
+    path('contato/', views.paginas_view, name='contato'),
 ]
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.contrib import messages
+from pages.models import Blocos
 
 # Create your views here.
 def index(request):
     context = {
         'menssagem': messages.success(request, 'Esta é uma mensagem de sucesso!')
     }
+    return render(request, 'index.html', context)
+
+
+def paginas_view(request):
+    url_name = request.resolver_match.url_name
+    pagina = {
+        'home': Blocos.objects.filter(pagina__nome='inicio',ativo=True).order_by('ordem'),
+        'sobre': Blocos.objects.filter(pagina__nome='sobre',ativo=True).order_by('ordem'),
+        'faq': Blocos.objects.filter(pagina__nome='faq',ativo=True).order_by('ordem'),
+        'contato': Blocos.objects.filter(pagina__nome='contato',ativo=True).order_by('ordem'),
+        }
+    context = {'blocos': pagina[str(url_name)]}
     return render(request, 'index.html', context)
