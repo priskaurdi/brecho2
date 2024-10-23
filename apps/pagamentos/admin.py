d18d9d2d2168b862968fe06896872a0095ade975
Priscilla Sequetin <55082190+priskaurdi@users.noreply.github.com>
2024-10-23 19:59:39 -0300

    ajustes_pgto_2024-10-23

--- apps/pagamentos/admin.py
+++ apps/pagamentos/admin.py
@@ -4,6 +4,6 @@ from .models import PagamentoPIX
 class PagamentoPIXAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'valor_total', 'status', 'data_pagamento')
     list_filter = ('status', 'data_pagamento')
-    search_fields = ('usuario__email', 'status')  # Pesquisa por email do usuário e status
+    search_fields = ('usuario__email', 'status', 'usuario__username', 'chave_pix')  # Pesquisa por email do usuário e status
 
 admin.site.register(PagamentoPIX, PagamentoPIXAdmin)
