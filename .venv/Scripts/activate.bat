b4395996335cac590227cf640381f64571c267f4
Priscilla Sequetin <55082190+priskaurdi@users.noreply.github.com>
2024-10-24 12:46:31 -0300

    jorge_pri_meeting_projeto_2024-10-24
    tentativa de acessar o projeto no hospital

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Priscila\Desktop\brecho2\.venv
+set VIRTUAL_ENV=C:\Users\Administrador\OneDrive - FUNDACAO ADIB JATENE\Documentos\GitHub\brecho2\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
